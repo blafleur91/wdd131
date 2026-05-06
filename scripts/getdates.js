395635d588ba59cb6a3570c39ad695f755d221bd
blafleur91 <blafleur@byupathway.edu>
2026-05-06 15:18:20 -0600

    added notes and added notes to explain what a line of code did in javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,6 @@ const today = new Date();
 currentyear.innerHTML = `${today.getFullYear()}`;
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
+// the above line of code seems super confusing, but really it is grabbing one of the titles of a piece in the html.
+// that is what the document.getElementById("lastModified").innerHTML is doing.
+// then the document.lastModified is a command. it outputs the information of when the document was last modified.
