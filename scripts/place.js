4e254730d388769776a2455821d2ce3f17ef0766
blafleur91 <blafleur@byupathway.edu>
2026-05-13 18:02:44 -0600

    began week03 assignment

--- scripts/place.js
+++ scripts/place.js
@@ -2,3 +2,9 @@ const today = new Date();
 currentyear.innerHTML = `${today.getFullYear()}`;
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+let temperature = 74;
+document.getElementById("temperature").textContent = `${temperature} °F`;
+
+let windSpeed = 10;
+document.getElementById("wind-speed").textContent = `${windSpeed} mph`;
