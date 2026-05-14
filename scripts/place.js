35ed533cc1d886396eb75f05bf88e4f72399c8f0
blafleur91 <blafleur@byupathway.edu>
2026-05-14 12:45:34 -0600

    first final upload for assignment for week03

--- scripts/place.js
+++ scripts/place.js
@@ -8,3 +8,14 @@ document.getElementById("temperature").textContent = `${temperature} °F`;
 
 let windSpeed = 10;
 document.getElementById("wind-speed").textContent = `${windSpeed} mph`;
+
+let windChill = "N/A";
+
+const calculateWindChill = (temp, speed) => Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16));
+
+
+if (temperature <= 50 && windSpeed > 3) {
+    windChill = calculateWindChill(temperature, windSpeed);
+}
+
+document.getElementById("wind-chill").textContent = windChill;
