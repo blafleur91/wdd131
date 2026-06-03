9cfb905ccb807b40b2c15df18363f8e87f2443a3
blafleur91 <blafleur@byupathway.edu>
2026-06-03 12:03:21 -0600

    reimplemented a feature for week 05 assignment that now just magically works.

--- scripts/review.js
+++ scripts/review.js
@@ -1,13 +1,13 @@
-// let reviewCount = localStorage.getItem("reviews");
+let reviewCount = localStorage.getItem("reviews");
 
-// // if nothing exists yet, start at 0
-// if (reviewCount === null) {
-//     reviewCount = 0;
-// }
+// if nothing exists yet, start at 0
+if (reviewCount === null) {
+    reviewCount = 0;
+}
 
 // apparently this will convert it from a string into a number
-// reviewCount = Number(reviewCount) + 1;
+reviewCount = Number(reviewCount) + 1;
 
-// localStorage.setItem("reviews", reviewCount);
+localStorage.setItem("reviews", reviewCount);
 
-// document.getElementById("reviewCount").textContent = reviewCount;
+document.getElementById("reviewCount").textContent = reviewCount;
